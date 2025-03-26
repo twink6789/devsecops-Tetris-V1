a49384a6c41847a9862b5c167e7e66a6ef7d83b7
twink6789 <twinkle.mohanta@gmail.com>
2025-03-26 16:17:37 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "devsecops-tetris-reyaz" # Replace with your actual S3 bucket name
+    bucket = "devsecops-tetris-twink" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "ap-south-1"
   }
